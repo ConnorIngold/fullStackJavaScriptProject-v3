d6cf1a32b4cf8af3c1cde193901af43d059f3352
connoringold <connoringold@gmail.com>
2019-01-26 17:50:59 +0000

    Adding files.

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const express = require("express");
+const path = require("path");
+const serveStatic = require("serve-static");
 
-const morgan = require('morgan');
-
-
+const morgan = require("morgan");
 
 app = express();
 app.use(serveStatic(__dirname + "/client/dist"));
 
 // app.use has to come after setting app to express
-app.use(morgan('combined'));
+app.use(morgan("combined"));
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error("Not found - " + req.originalUrl);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.status || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started ' + port);
+console.log("server started " + port);
