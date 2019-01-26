d7a5ae941d1252fe360686861116a2067790e91e
connoringold <connoringold@gmail.com>
2019-01-26 16:57:35 +0000

    Adding files.

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const serveStatic = require('serve-static');
 
 const morgan = require('morgan');
 
-app.use(morgan(dev));
+
 
 app = express();
 app.use(serveStatic(__dirname + "/client/dist"));
+
+// app.use has to come after setting app to express
+app.use(morgan(dev));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
